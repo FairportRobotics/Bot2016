0aecc8c16e82e5bf9c0707442d0132d7294349cc
astussman88 <misty.seika@gmail.com>
2016-01-28 19:52:55 -0500

    Check-in add the DriveSubsystem base.

---
+++ src/org/usfirst/frc/team578/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team578.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DriveSubsystem extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
