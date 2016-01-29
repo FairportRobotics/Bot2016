af540fb92ceb17bbaf006b12615f24de11554db0
abush3 <alexbush410@gmail.com>
2016-01-28 19:48:54 -0500

    Assign the talons for driving ID numbers

--- src/org/usfirst/frc/team578/robot/RobotMap.java
+++ src/org/usfirst/frc/team578/robot/RobotMap.java
@@ -15,4 +15,11 @@ public class RobotMap {
     // number and the module. For example you with a rangefinder:
     // public static int rangefinderPort = 1;
     // public static int rangefinderModule = 1;
+	
+	public static final int RIGHT_MASTER=16;
+	public static final int RIGHT_SLAVE=15;
+	public static final int LEFT_MASTER=14;
+	public static final int LEFT_SLAVE=13;
+	
+	
 }
