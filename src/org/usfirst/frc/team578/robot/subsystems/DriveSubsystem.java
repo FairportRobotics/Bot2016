1f19ce99b178e3c0b011a4ec6ef557cef99a4aa2
wildog4 <wildog4@hotmail.com>
2016-01-28 19:57:28 -0500

    added CANtalon variables

--- src/org/usfirst/frc/team578/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team578/robot/subsystems/DriveSubsystem.java
@@ -1,9 +1,15 @@
 package org.usfirst.frc.team578.robot.subsystems;
 
+import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class DriveSubsystem extends Subsystem {
-
+	
+	private CANTalon rightMaster;
+	private CANTalon rightSlave;
+	private CANTalon leftMaster;
+	private CANTalon leftSlave;
+	
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
