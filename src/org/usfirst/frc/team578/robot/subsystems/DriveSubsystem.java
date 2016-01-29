bda21be0fd8fdbf15e5bd81dc8587ff4d9c75449
abush3 <alexbush410@gmail.com>
2016-01-28 20:06:55 -0500

    initialize method for the driving talons

--- src/org/usfirst/frc/team578/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team578/robot/subsystems/DriveSubsystem.java
@@ -1,5 +1,7 @@
 package org.usfirst.frc.team578.robot.subsystems;
 
+import org.usfirst.frc.team578.robot.RobotMap;
+
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
@@ -14,6 +16,13 @@ public class DriveSubsystem extends Subsystem {
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public void initialize(){
+		rightMaster= new CANTalon(RobotMap.RIGHT_MASTER);
+		rightSlave= new CANTalon(RobotMap.RIGHT_SLAVE);
+		leftMaster= new CANTalon(RobotMap.LEFT_MASTER);
+		leftSlave= new CANTalon(RobotMap.LEFT_SLAVE);
 	}
 
 }
