87dc2dd2227fe25ba79b74917e12cba04ca938f7
abush3 <alexbush410@gmail.com>
2016-01-30 10:00:52 -0500

    Enabling control FOR THE DRIVE TALONS

--- src/org/usfirst/frc/team578/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team578/robot/subsystems/DriveSubsystem.java
@@ -6,42 +6,47 @@ import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class DriveSubsystem extends Subsystem {
-	
+
 	private CANTalon rightMaster;
 	private CANTalon rightSlave;
 	private CANTalon leftMaster;
 	private CANTalon leftSlave;
-	
+
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
 
 	}
-	
-	public void initialize(){
-		rightMaster= new CANTalon(RobotMap.RIGHT_MASTER);
-		rightSlave= new CANTalon(RobotMap.RIGHT_SLAVE);
-		leftMaster= new CANTalon(RobotMap.LEFT_MASTER);
-		leftSlave= new CANTalon(RobotMap.LEFT_SLAVE);
-		
+
+	public void initialize() {
+		rightMaster = new CANTalon(RobotMap.RIGHT_MASTER);
+		rightSlave = new CANTalon(RobotMap.RIGHT_SLAVE);
+		leftMaster = new CANTalon(RobotMap.LEFT_MASTER);
+		leftSlave = new CANTalon(RobotMap.LEFT_SLAVE);
+
 		rightMaster.setSafetyEnabled(true);
 		rightSlave.setSafetyEnabled(true);
 		leftMaster.setSafetyEnabled(true);
 		leftSlave.setSafetyEnabled(true);
-		
+
 		rightMaster.setExpiration(.25);
 		rightSlave.setExpiration(.25);
 		leftMaster.setExpiration(.25);
 		leftSlave.setExpiration(.25);
-		
+
 		rightMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
 		leftMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
-		
+
 		rightSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
 		leftSlave.changeControlMode(CANTalon.TalonControlMode.Follower);
-		
+
 		rightSlave.set(RobotMap.RIGHT_MASTER);
 		leftSlave.set(RobotMap.LEFT_MASTER);
+
+		leftMaster.enableControl();
+		leftSlave.enableControl();
+		rightMaster.enableControl();
+		rightSlave.enableControl();
 	}
 
 }
