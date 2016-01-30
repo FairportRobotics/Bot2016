45b420120f6ad1b2f3c218c4077bc9611424d0f0
TonyStark <icloudisbetta@gmail.com>
2016-01-30 10:06:06 -0500

    set MASTERS TO 0

--- src/org/usfirst/frc/team578/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team578/robot/subsystems/DriveSubsystem.java
@@ -47,6 +47,9 @@ public class DriveSubsystem extends Subsystem {
 		leftSlave.enableControl();
 		rightMaster.enableControl();
 		rightSlave.enableControl();
+
+		rightMaster.set(0);
+		leftMaster.set(0);
 	}
 
 }
