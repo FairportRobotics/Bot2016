b2589f1978acce4ac25f2d72f9e0281ca1661099
JakeHatesFurries42069 <jakepatrickcicero@gmail.com>
2016-01-30 10:18:25 -0500

    defines joysticks addresses

--- src/org/usfirst/frc/team578/robot/RobotMap.java
+++ src/org/usfirst/frc/team578/robot/RobotMap.java
@@ -1,4 +1,5 @@
 package org.usfirst.frc.team578.robot;
+
 /**
  * The RobotMap is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
@@ -6,20 +7,22 @@ package org.usfirst.frc.team578.robot;
  * floating around.
  */
 public class RobotMap {
-    // For example to map the left and right motors, you could define the
-    // following variables to use with your drivetrain subsystem.
-    // public static int leftMotor = 1;
-    // public static int rightMotor = 2;
-    
-    // If you are using multiple modules, make sure to define both the port
-    // number and the module. For example you with a rangefinder:
-    // public static int rangefinderPort = 1;
-    // public static int rangefinderModule = 1;
-	
-	public static final int RIGHT_MASTER=16;
-	public static final int RIGHT_SLAVE=15;
-	public static final int LEFT_MASTER=14;
-	public static final int LEFT_SLAVE=13;
-	
-	
+	// For example to map the left and right motors, you could define the
+	// following variables to use with your drivetrain subsystem.
+	// public static int leftMotor = 1;
+	// public static int rightMotor = 2;
+
+	// If you are using multiple modules, make sure to define both the port
+	// number and the module. For example you with a rangefinder:
+	// public static int rangefinderPort = 1;
+	// public static int rangefinderModule = 1;
+
+	public static final int RIGHT_MASTER = 16;
+	public static final int RIGHT_SLAVE = 15;
+	public static final int LEFT_MASTER = 14;
+	public static final int LEFT_SLAVE = 13;
+
+	public static final int LEFT_JOYSTICK = 0;
+	public static final int RIGHT_JOYSTICK = 1;
+
 }
