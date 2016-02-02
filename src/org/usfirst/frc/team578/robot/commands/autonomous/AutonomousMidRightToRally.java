6eda42db2e088c763638ba8d3ff1c1f2a4770547
BenderIsGreat003 <jakepatrickcicero@gmail.com>
2016-02-01 20:09:30 -0500

    created AutoMidRightRally

---
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousMidRightToRally.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team578.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutonomousMidRightToRally extends CommandGroup {
+
+	public AutonomousMidRightToRally() {
+
+		// The idea here is to move to a common (known)
+		// point on the field and be ready for next phase
+
+		// addSequential(new AutonomousDriveCommand(.5, .5, 5));
+	}
+
+}
