01c9ede2ee8935dd00e8016432085cc7dab00527
astussman88 <misty.seika@gmail.com>
2016-02-01 20:13:29 -0500

    Added Scoring Automous mode classes

---
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousScoringLeft.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team578.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutonomousScoringLeft extends CommandGroup {
+
+	public AutonomousScoringLeft() {
+
+	}
+
+}
---
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousScoringNone.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team578.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutonomousScoringNone extends CommandGroup {
+
+	public AutonomousScoringNone() {
+
+		// The idea here is to move to a common (known)
+		// point on the field and be ready for next phase
+
+		// addSequential(new AutonomousDriveCommand(.5, .5, 5));
+	}
+
+}
