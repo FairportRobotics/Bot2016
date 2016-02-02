645861fb77d530a393a19048dbef5480eab950cb
Kevin <h1d3m3@yahoo.com>
2016-02-01 19:49:44 -0500

    initial master class to run each part of the autonomous mode.

---
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousMaster.java
@@ -0,0 +1,15 @@
+package org.usfirst.frc.team578.robot.commands.autonomous;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutonomousMaster extends CommandGroup {
+
+	public AutonomousMaster(Command crossing, Command movingToRally,
+			Command scoring) {
+
+		addSequential(crossing);
+		addSequential(movingToRally);
+		addSequential(scoring);
+	}
+}
