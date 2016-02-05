0658f224b1a6a511aa319a8bfe058cb7b0af3b26
astussman88 <misty.seika@gmail.com>
2016-02-04 20:00:35 -0500

    Added comments and stop to Scoring Left

--- src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousScoringLeft.java
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousScoringLeft.java
@@ -6,12 +6,13 @@ public class AutonomousScoringLeft extends CommandGroup {
 
 	public AutonomousScoringLeft() {
 
-		addSequential(new AutonomousDriveCommand(-1, 1, .1));
-		addSequential(new AutonomousDriveCommand(1, 1, .76));
-		addSequential(new AutonomousDriveCommand(1, -1, .1));
-		addSequential(new AutonomousDriveCommand(1, 1, .88));
-		addSequential(new AutonomousDriveCommand(1, -1, .067));
-		addSequential(new AutonomousDriveCommand(1, 1, .47));
+		addSequential(new AutonomousDriveCommand(-1, 1, .1)); // turn left
+		addSequential(new AutonomousDriveCommand(1, 1, .76)); // forward 6.5 ft
+		addSequential(new AutonomousDriveCommand(1, -1, .1)); // turn right
+		addSequential(new AutonomousDriveCommand(1, 1, .88)); // forward 7.54 ft
+		addSequential(new AutonomousDriveCommand(1, -1, .067)); // turn right
+		addSequential(new AutonomousDriveCommand(1, 1, .47)); // forward 4 ft
+		addSequential(new AutonomousDriveCommand(0, 0, .1)); // stop
 
 	}
 
