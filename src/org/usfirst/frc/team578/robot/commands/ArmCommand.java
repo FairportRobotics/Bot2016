f84cf065667d4b194a00e03327783bbbb9760573
astussman88 <misty.seika@gmail.com>
2016-02-06 10:55:55 -0500

    Made stop max and min vals more accessible.

--- src/org/usfirst/frc/team578/robot/commands/ArmCommand.java
+++ src/org/usfirst/frc/team578/robot/commands/ArmCommand.java
@@ -6,6 +6,9 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class ArmCommand extends Command {
 
+	public static final double MAX_STOP_BUFFER = 0.3;
+	public static final double MIN_STOP_BUFFER = 0.3;
+
 	public ArmCommand() {
 		requires(Robot.armSubsystem);
 	}
@@ -20,14 +23,11 @@ public class ArmCommand extends Command {
 
 		double leftJoystickValue = Robot.oi.getLeft();
 
-		double maxStopBuffer = .3;
-		double minStopBuffer = -.3;
-
-		if (leftJoystickValue > maxStopBuffer) {
+		if (leftJoystickValue > MAX_STOP_BUFFER) {
 			// forward
 			Robot.armSubsystem.forward(leftJoystickValue);
 
-		} else if (leftJoystickValue < minStopBuffer) {
+		} else if (leftJoystickValue < MIN_STOP_BUFFER) {
 			// backwards will negate any value passed to it
 			// so we need to make sure the joystick value is positivez
 			Robot.armSubsystem.backwards(Math.abs(leftJoystickValue));
