fb8a2bdbf438353475120106876b39b090b44e4e
BenderIsGreat003 <jakepatrickcicero@gmail.com>
2016-02-06 10:04:12 -0500

    adds scoring to scoring left

--- src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousScoringLeft.java
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousScoringLeft.java
@@ -17,6 +17,7 @@ public class AutonomousScoringLeft extends CommandGroup {
 		addSequential(new AutonomousDriveCommand(1, -1, .067)); // turn right
 		addSequential(new AutonomousDriveCommand(0, 0, .1)); // stop
 		addSequential(new AutonomousDriveCommand(1, 1, .47)); // forward 4 ft
+		addSequential(new AutonomousOutputCommand());// shoots ball
 		addSequential(new AutonomousDriveCommand(0, 0, .1)); // stop
 
 	}
