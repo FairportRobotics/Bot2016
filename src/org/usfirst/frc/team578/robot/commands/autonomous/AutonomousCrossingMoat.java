6d58846fbd958d5195aa65438696989102b2e30a
astussman88 <misty.seika@gmail.com>
2016-02-13 13:23:15 -0500

    added time to drive

--- src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousCrossingMoat.java
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousCrossingMoat.java
@@ -7,8 +7,8 @@ public class AutonomousCrossingMoat extends CommandGroup {
 	public AutonomousCrossingMoat() {
 
 		addSequential(new AutonomousDriveToPitchZero(1, 1));
+		addSequential(new AutonomousDriveCommand(1, 1, .173));
 		addSequential(new AutonomousTurntoZeroCommand());
 
 	}
-
 }
--- src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousCrossingRamparts.java
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousCrossingRamparts.java
@@ -7,8 +7,8 @@ public class AutonomousCrossingRamparts extends CommandGroup {
 	public AutonomousCrossingRamparts() {
 
 		addSequential(new AutonomousDriveToPitchZero(1, 1));
+		addSequential(new AutonomousDriveCommand(1, 1, .173));
 		addSequential(new AutonomousTurntoZeroCommand());
 
 	}
-
 }
