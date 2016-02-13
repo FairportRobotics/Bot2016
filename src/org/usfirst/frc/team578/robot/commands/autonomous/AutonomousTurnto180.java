8f554b2eb4bb5f17de8bf7cacd51ef0e50690ea9
OpenSourceBread <jakepatrickcicero@gmail.com>
2016-02-13 11:20:40 -0500

    Added AutonomousTurnto180

---
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousTurnto180.java
@@ -0,0 +1,53 @@
+package org.usfirst.frc.team578.robot.commands.autonomous;
+
+import org.usfirst.frc.team578.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class AutonomousTurnto180 extends Command {
+	private double error = 2.0;
+
+	public AutonomousTurnto180() {
+		requires(Robot.driveSubsystem);
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		if (Robot.navx.getFusedHeading() > 0 - error
+				&& Robot.navx.getFusedHeading() < 180 + error) {
+			Robot.driveSubsystem.drive(0, 0);
+		} else if (Robot.navx.getFusedHeading() <= 180 - error
+				|| Robot.navx.getFusedHeading() > 0) {
+			Robot.driveSubsystem.drive(1, -1);
+		} else {
+			Robot.driveSubsystem.drive(-1, 1);
+		}
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return (Robot.navx.getFusedHeading() > 180 - error && Robot.navx
+				.getFusedHeading() < 180 + error);
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
