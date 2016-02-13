455624f663818125fb1b68b516e86e8bb8cef2ba
astussman88 <misty.seika@gmail.com>
2016-02-13 15:33:49 -0500

    Renamed class to make it more compliant

--- src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousTurnTo120.java
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousTurnTo120.java
@@ -4,27 +4,22 @@ import org.usfirst.frc.team578.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
 
-public class Turnto120 extends Command {
+public class AutonomousTurnTo120 extends Command {
 	private double error = 2.0;
 
-	public Turnto120() {
+	public AutonomousTurnTo120() {
 		requires(Robot.driveSubsystem);
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
-		if (Robot.navx.getFusedHeading() > 120 - error
-				&& Robot.navx.getFusedHeading() < 120 + error) {
+		if (Robot.navx.getFusedHeading() > 120 - error && Robot.navx.getFusedHeading() < 120 + error) {
 			Robot.driveSubsystem.drive(0, 0);
-		} else if (Robot.navx.getFusedHeading() <= 300
-				|| Robot.navx.getFusedHeading() > 120 + error) {
+		} else if (Robot.navx.getFusedHeading() <= 300 || Robot.navx.getFusedHeading() > 120 + error) {
 			Robot.driveSubsystem.drive(1, -1);
 		} else {
 			Robot.driveSubsystem.drive(-1, 1);
@@ -33,21 +28,15 @@ public class Turnto120 extends Command {
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
-		return (Robot.navx.getFusedHeading() > 120 - error && Robot.navx
-				.getFusedHeading() < 120 + error);
+		return (Robot.navx.getFusedHeading() > 120 - error && Robot.navx.getFusedHeading() < 120 + error);
 	}
 
 	@Override
 	protected void end() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	protected void interrupted() {
-		// TODO Auto-generated method stub
-
 	}
 
 }
