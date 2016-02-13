e996f84f33f430bec839fa696bf93193319717fd
TonyStark <icloudisbetta@gmail.com>
2016-02-13 13:35:42 -0500

    added comments to Crossing RoughTerrain and Crossing Ramparts

--- src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousCrossingRockWall.java
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousCrossingRockWall.java
@@ -5,9 +5,11 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousCrossingRockWall extends CommandGroup {
 
 	public AutonomousCrossingRockWall() {
-
+		// moves until flat detected [down]
 		addSequential(new AutonomousDriveToPitchZero(1, 1));
+		// moves from edge of O.W. to Alignment Line [down]
 		addSequential(new AutonomousDriveCommand(1, 1, .173));
+		// sets robot roll to 0 degrees [down]
 		addSequential(new AutonomousTurntoZeroCommand());
 	}
 
--- src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousCrossingRoughTerrain.java
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousCrossingRoughTerrain.java
@@ -5,9 +5,11 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutonomousCrossingRoughTerrain extends CommandGroup {
 
 	public AutonomousCrossingRoughTerrain() {
-
+		// moves until flat detected [down]
 		addSequential(new AutonomousDriveToPitchZero(1, 1));
+		// moves from edge of O.W. to Alignment Line [down]
 		addSequential(new AutonomousDriveCommand(1, 1, .173));
+		// sets robot roll to 0 degrees [down]
 		addSequential(new AutonomousTurntoZeroCommand());
 	}
 
