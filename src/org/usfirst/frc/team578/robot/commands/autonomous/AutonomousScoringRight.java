7b3fdc09dab48dbc21a6c2bf041058a0a2531700
OpenSourceBread <jakepatrickcicero@gmail.com>
2016-02-13 12:47:55 -0500

    fixed autoscore right

--- src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousScoringRight.java
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousScoringRight.java
@@ -10,23 +10,23 @@ public class AutonomousScoringRight extends CommandGroup {
 		// point on the field and be ready for next phase
 
 		// addSequential(new AutonomousDriveCommand(.5, .5, 5));
-		addSequential(new AutonomousDriveCommand(1, -1, .1));// turn right 90
-																// degrees
-		addSequential(new AutonomousDriveCommand(0, 0, 0));// stops
+		addSequential(new AutonomousTurnRight());// turn right 90
+													// degrees
+		// addSequential(new AutonomousDriveCommand(0, 0, 0));// stops
 		addSequential(new AutonomousDriveCommand(1, 1, .76));// forward 6.5 feet
 		addSequential(new AutonomousDriveCommand(0, 0, 0));// stops
-		addSequential(new AutonomousDriveCommand(-1, 1, .1));// turn left 90
-																// degrees
-		addSequential(new AutonomousDriveCommand(0, 0, 0));// stops
+		addSequential(new AutonomousTurntoZeroCommand());// turn left 90
+															// degrees
+		// addSequential(new AutonomousDriveCommand(0, 0, 0));// stops
 		addSequential(new AutonomousDriveCommand(1, 1, .88));// forward 7.54
 																// feet
-		addSequential(new AutonomousDriveCommand(0, 0, 0));// stops
-		addSequential(new AutonomousDriveCommand(-1, 1, .067));// turn left 60
-																// degrees
-		addSequential(new AutonomousDriveCommand(0, 0, 0));// stops
-		addSequential(new AutonomousDriveCommand(1, 1, .47));// forward 4 feet
+		// addSequential(new AutonomousDriveCommand(0, 0, 0));// stops
+		addSequential(new Turnto120());// turn left 60
+										// degrees
+		// addSequential(new AutonomousDriveCommand(0, 0, 0));// stops
+		addSequential(new AutonomousDriveCommand(-1, -1, .47));// forward 4 feet
 		addSequential(new AutonomousOutputCommand());// shoots out the ball
-		addSequential(new AutonomousDriveCommand(0, 0, 0));// stops
+		// addSequential(new AutonomousDriveCommand(0, 0, 0));// stops
 
 	}
 
