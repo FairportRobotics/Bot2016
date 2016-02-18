879e923869f03ad5b23a14d8d6f62dc3cb3014a8
wildog4 <wildog4@hotmail.com>
2016-02-17 20:01:36 -0500

    cleaned up code

--- src/org/usfirst/frc/team578/robot/commands/IntakeCommand.java
+++ src/org/usfirst/frc/team578/robot/commands/IntakeCommand.java
@@ -7,38 +7,30 @@ import edu.wpi.first.wpilibj.command.Command;
 public class IntakeCommand extends Command {
 
 	public IntakeCommand() {
-		// TODO Auto-generated constructor stub
 		requires(Robot.intakeSubsystem);
 	}
 
 	@Override
 	protected void initialize() {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
 		Robot.intakeSubsystem.spinIntake();
-
 	}
 
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
 	protected void end() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	protected void interrupted() {
-		// TODO Auto-generated method stub
 		Robot.intakeSubsystem.spinStop();
 
 	}
