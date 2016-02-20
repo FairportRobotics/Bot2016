869b724013e55b954c7e1f895a75a4abf8446483
astussman88 <misty.seika@gmail.com>
2016-02-20 13:46:36 -0500

    added execute body

--- src/org/usfirst/frc/team578/robot/commands/BallSensorCommand.java
+++ src/org/usfirst/frc/team578/robot/commands/BallSensorCommand.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team578.robot.commands;
 import org.usfirst.frc.team578.robot.Robot;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class BallSensorCommand extends Command {
 
@@ -17,6 +18,8 @@ public class BallSensorCommand extends Command {
 	@Override
 	protected void execute() {
 
+		boolean BallSensor = Robot.ballSensorSubsystem.get();
+		SmartDashboard.putBoolean("Ball Sensor", BallSensor);
 	}
 
 	@Override
