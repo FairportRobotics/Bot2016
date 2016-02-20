11a3e3ba45eab7f06b2cd663798dee6d103d8798
wildog4 <wildog4@hotmail.com>
2016-02-20 14:25:24 -0500

    Made commands to extend and retract the winch by holding a button.

---
+++ src/org/usfirst/frc/team578/robot/commands/WinchTestRetractCommand.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team578.robot.commands;
+
+import org.usfirst.frc.team578.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class WinchTestRetractCommand extends Command {
+
+	public WinchTestRetractCommand() {
+		requires(Robot.winchSubsystem);
+	}
+
+	@Override
+	protected void initialize() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void execute() {
+		// TODO Auto-generated method stub
+		Robot.winchSubsystem.retract();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected void end() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void interrupted() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
