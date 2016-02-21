955ee119f36e7eb6b593adda92523dfb9a2aaee6
Kevin <h1d3m3@yahoo.com>
2016-02-21 15:15:55 -0500

    fixed 240 code

--- src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousTurnTo240.java
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/AutonomousTurnTo240.java
@@ -5,7 +5,8 @@ import org.usfirst.frc.team578.robot.Robot;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class AutonomousTurnTo240 extends Command {
-	private double error = 2.0;
+	private double error = .25;
+	private boolean zeroFound = false;
 
 	@Override
 	protected void initialize() {
@@ -15,20 +16,45 @@ public class AutonomousTurnTo240 extends Command {
 
 	@Override
 	protected void execute() {
-		// TODO Auto-generated method stub
-		if ((Robot.navx.getFusedHeading() > (240 - error)) && (Robot.navx.getFusedHeading() < (240 + error))) {
+
+		double currentHeading = Robot.navx.getFusedHeading();
+		double MIN_LEFT_VAL = 240 - error;
+		double MIN_RIGHT_VAL = 240 + error;
+
+		System.err.println("heading : " + currentHeading + " : " + zeroFound);
+
+		if (zeroFound) {
 			Robot.driveSubsystem.drive(0, 0);
-		} else if ((Robot.navx.getFusedHeading() >= (240 + error)) || (Robot.navx.getFusedHeading() < 60)) {
-			Robot.driveSubsystem.drive(.4, -.4);
-		} else {
-			Robot.driveSubsystem.drive(-.4, .4);
+			return;
 		}
+
+		if (currentHeading > MIN_LEFT_VAL && currentHeading < MIN_RIGHT_VAL) {
+			Robot.driveSubsystem.drive(0, 0);
+			zeroFound = true;
+
+		} else if (currentHeading > 60 && currentHeading < 240) {
+			Robot.driveSubsystem.drive(.4, -.4); // left turn - increase
+			// heading
+		} else {
+			Robot.driveSubsystem.drive(-.4, .4); // right turn - decrease
+			// heading
+		}
+		// // TODO Auto-generated method stub
+		// if ((Robot.navx.getFusedHeading() > (240 - error)) &&
+		// (Robot.navx.getFusedHeading() < (240 + error))) {
+		// Robot.driveSubsystem.drive(0, 0);
+		// } else if ((Robot.navx.getFusedHeading() >= (240 + error)) ||
+		// (Robot.navx.getFusedHeading() < 60)) {
+		// Robot.driveSubsystem.drive(.4, -.4);
+		// } else {
+		// Robot.driveSubsystem.drive(-.4, .4);
+		// }
 	}
 
 	@Override
 	protected boolean isFinished() {
 		// TODO Auto-generated method stub
-		return ((Robot.navx.getFusedHeading() > (240 - error)) && (Robot.navx.getFusedHeading() < (240 + error)));
+		return zeroFound;
 	}
 
 	@Override
