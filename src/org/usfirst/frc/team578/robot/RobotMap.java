c6986c40c1784e39abd86671f907303c6210ffa4
Kevin <h1d3m3@yahoo.com>
2016-02-22 20:30:54 -0500

    added comment

--- src/org/usfirst/frc/team578/robot/RobotMap.java
+++ src/org/usfirst/frc/team578/robot/RobotMap.java
@@ -33,6 +33,7 @@ public class RobotMap {
 	public static final int GAMEPAD = 2;
 	public static final int INTAKE_BUTTON = 3;
 
+	// These are the default speeds used to turn
 	public static final double DEFAULT_LEFT_TURN_SPEED = 0.6;
 	public static final double DEFAULT_RIGHT_TURN_SPEED = 0.6;
 
