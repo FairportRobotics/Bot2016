08479163474e87984ead985a6a86b04dc5331a88
Kevin <h1d3m3@yahoo.com>
2016-03-16 20:30:29 -0400

    Added distances/times for each position to left and right rally points. Created left rally and right rally to left and right score. Wired up left and right scoring to chooser.

--- src/org/usfirst/frc/team578/robot/commands/autonomous/scoring/AutonomousScoringRight.java
+++ src/org/usfirst/frc/team578/robot/commands/autonomous/scoring/AutonomousScoringRight.java
@@ -17,19 +17,19 @@ public class AutonomousScoringRight extends CommandGroup {
 		// point on the field and be ready for next phase
 
 		addSequential(new AutonomousTurnRight(RobotMap.DEFAULT_LEFT_WHEEL_TURN_SPEED, RobotMap.DEFAULT_RIGHT_WHEEL_TURN_SPEED));// turn
-																													// right
-																													// 90
+		// right
+		// 90
 		// degrees
 		addSequential(new AutonomousDriveCommand(1, 1, .86));// forward 6.5 feet
 		addSequential(new AutonomousTurntoZeroCommand(RobotMap.DEFAULT_LEFT_WHEEL_TURN_SPEED, RobotMap.DEFAULT_RIGHT_WHEEL_TURN_SPEED));// turn
-																															// left
-																															// 90
+		// left
+		// 90
 		// degrees
 		addSequential(new AutonomousDriveCommand(1, 1, .88));// forward 7.54
 																// feet
 		addSequential(new AutonomousTurnTo120(RobotMap.DEFAULT_LEFT_WHEEL_TURN_SPEED, RobotMap.DEFAULT_RIGHT_WHEEL_TURN_SPEED));// turn
-																													// left
-																													// 60
+		// left
+		// 60
 		// degrees
 		addSequential(new AutonomousDriveCommand(-1, -1, .47));// forward 4 feet
 		addSequential(new AutonomousOutputCommand());// shoots out the ball
